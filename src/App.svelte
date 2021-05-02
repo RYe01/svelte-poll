96c5f08967904af2a7e2954d93c6916647875145
RYe01 <rozs0001@hz.nl>
2021-05-03 00:40:15 +0200

    header and footer

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Header from './components/Header.svelte';
+	import Footer from './components/Footer.svelte';
+</script>
+
+<Header />
+<main>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe distinctio eos ad similique, aperiam ab deserunt, veritatis sint facilis ea ipsa. Quos tenetur reiciendis minima aperiam reprehenderit molestias vitae ipsa!</p>
+</main>
+<Footer />
+
+<style>
+	main {
+		max-width: 690px;
+		margin: 40px auto;
+	}
+</style>
