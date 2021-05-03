87401077c6484cb91d82ce3c16c4bd32efb44769
RYe01 <rozs0001@hz.nl>
2021-05-03 18:58:22 +0200

    voting on polls work

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,7 @@
 	import Footer from './components/Footer.svelte';
 	import Tabs from './shared/Tabs.svelte';
 	import CreatePollForm from './components/CreatePollForm.svelte';
+	import PollList from './components/PollList.svelte';
 
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
@@ -27,13 +28,29 @@
 
 		activeItem = 'Current Polls';
 	}
+
+	const handleVote = (e) => {
+		const { id, option } = e.detail;
+
+		let copiedPolls = [...polls];
+		let upvotedPoll = copiedPolls.find((poll) => poll.id == id);
+
+		if (option === 'a') {
+			upvotedPoll.votesA++;
+		}
+		if (option === 'b') {
+			upvotedPoll.votesB++;
+		}
+
+		polls = copiedPolls;
+	}
 </script>
 
 <Header />
 <main>
 	<Tabs {activeItem} {items} on:tabChange={tabChange} />
 	{#if activeItem === 'Current Polls'}
-		<p>This tab will show all the current polls</p>
+		<PollList {polls} on:vote={handleVote} />
 	{:else if activeItem === 'Add New Poll'}
 		<CreatePollForm on:add={handleAdd} />
 	{/if}
