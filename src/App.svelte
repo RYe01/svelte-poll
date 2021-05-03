51a84f3ec9140fd36a2e0a7eaf03a215b49caa55
RYe01 <rozs0001@hz.nl>
2021-05-03 16:14:25 +0200

    poll form added

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,30 @@
 	import Header from './components/Header.svelte';
 	import Footer from './components/Footer.svelte';
 	import Tabs from './shared/Tabs.svelte';
+	import CreatePollForm from './components/CreatePollForm.svelte';
 
 	let items = ['Current Polls', 'Add New Poll'];
 	let activeItem = 'Current Polls';
 	const tabChange = (e) => {
 		activeItem = e.detail;
+	}
+
+	let polls = [
+		{
+			id: 1,
+			question: 'A or B?',
+			answerA: 'A',
+			answerB: 'B',
+			votesA: 9,
+			votesB: 15,
+		},
+	];
+
+	const handleAdd = (e) => {
+		const poll = e.detail;
+		polls = [poll, ...polls];
+
+		activeItem = 'Current Polls';
 	}
 </script>
 
@@ -16,7 +35,7 @@
 	{#if activeItem === 'Current Polls'}
 		<p>This tab will show all the current polls</p>
 	{:else if activeItem === 'Add New Poll'}
-		<p>This tab will let you add a new poll</p>
+		<CreatePollForm on:add={handleAdd} />
 	{/if}
 </main>
 <Footer />
